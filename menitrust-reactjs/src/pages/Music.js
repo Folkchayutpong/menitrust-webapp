4131ea192cf65625afcff8df4cd30cb2dc2100af
Folkchayutpong <folkchayutpong@gmail.com>
2024-07-31 21:31:31 +0700

    Do Music page a little bit

--- menitrust-reactjs/src/pages/Music.js
+++ menitrust-reactjs/src/pages/Music.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import '../styles/Music.css'
 
 function Music() {
   return (
-    <div>
-      
+    <div className='music-container'>
+      <h1 className='titleA'>Albums</h1>
+      <div className='albums'>
+        <div className='anAlbum'>
+          <img src='' />
+          <h2></h2>
+        </div>
+        <div className='anAlbum'>
+          <img src='' />
+          <h2></h2>
+        </div>
+        <div className='anAlbum'>
+          <img src='' />
+          <h2></h2>
+        </div>
+        <div className='anAlbum'>
+          <img src='' />
+          <h2></h2>
+        </div>
+      </div>
+      <h1 className='titleB'>Extended Plays</h1>
+      <div className='eps'>
+        <div className='anEp'><img src='' />
+          <h2></h2>
+          </div>
+        <div className='anEp'><img src='' />
+          <h2></h2>
+          </div>
+
+      </div>
     </div>
   )
 }
